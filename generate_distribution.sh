5b210a588b0b5a541c7ea9321b7db5819ebf03e0
Chitanda-Satou <shinji006@126.com>
2020-06-30 10:43:34 +0800

    test

--- generate_distribution.sh
+++ generate_distribution.sh
@@ -3,6 +3,10 @@
 # generate distribution archives as:
 # https://packaging.python.org/tutorials/packaging-projects/
 
-python3 -m pip install --user --upgrade setuptools wheel
-
+# python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
+
+
+# Uploading the distribution archives
+# python3 -m pip install --user --upgrade twine
+python3 -m twine upload dist/*
